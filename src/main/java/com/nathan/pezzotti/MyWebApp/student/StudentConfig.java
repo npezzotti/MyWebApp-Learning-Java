c210a61995cad888f7128a0ef4e8719f5b0569b5
npezzotti <nathan.pezzotti@datadoghq.com>
2021-03-15 22:42:57 -0400

    Added CommandLineRunner

---
+++ src/main/java/com/nathan/pezzotti/MyWebApp/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.nathan.pezzotti.MyWebApp.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student nathan = new Student(
+                    "Nathan",
+                    "nathan.pezzotti@email.com",
+                    LocalDate.of(1993, Month.AUGUST, 22),
+                    27
+            );
+            Student jesse = new Student(
+                    "Jesse",
+                    "jesse.lee@email.com",
+                    LocalDate.of(1994, Month.MARCH, 12),
+                    27
+            );
+
+            repository.saveAll(
+                    List.of(nathan, jesse)
+            );
+        };
+    }
+}
