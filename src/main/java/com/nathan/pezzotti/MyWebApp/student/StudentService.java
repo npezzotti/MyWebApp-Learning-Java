c4e2ee225216e2978199df161dac1c8594f7c4ec
npezzotti <nathan.pezzotti@datadoghq.com>
2021-03-15 21:37:55 -0400

    Added jpa data layer

--- src/main/java/com/nathan/pezzotti/MyWebApp/student/StudentService.java
+++ src/main/java/com/nathan/pezzotti/MyWebApp/student/StudentService.java
@@ -1,22 +1,20 @@
 package com.nathan.pezzotti.MyWebApp.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> GetStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Nathan",
-                        "nathan.pezzotti@email.com",
-                        LocalDate.of(1993, Month.AUGUST, 22),
-                        27
-                )
-        );
+        return studentRepository.findAll();
     }
 }
