9334117b2af0bb2dbf8e3eede632fab11f318d34
npezzotti <nathan.pezzotti@datadoghq.com>
2021-03-16 21:17:49 -0400

    Added PostMapping

--- src/main/java/com/nathan/pezzotti/MyWebApp/student/StudentService.java
+++ src/main/java/com/nathan/pezzotti/MyWebApp/student/StudentService.java
@@ -17,4 +17,8 @@ public class StudentService {
     public List<Student> GetStudents() {
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
